d9468471a35a573bfbb6370d1f41a8e3edd5c308
Dustin <dustin.anthony.coffman@gmail.com>
2019-12-17 19:49:31 -0600

    feeding form

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Bird,Photo
+from .models import Bird, Photo
 from .forms import FeedingForm
 import uuid
 import boto3
@@ -24,6 +24,14 @@ class BirdDelete(DeleteView):
     model = Bird
     success_url = '/birds/'
 
+def add_feeding(request, bird_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
+
 def home(request):
     return render(request, 'index.html')
 
@@ -40,7 +48,8 @@ def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
     feeding_form = FeedingForm()
     return render(request, 'birds/detail.html', {
-        'bird': bird, 'feeding_form': feeding_form
+        'bird': bird, 
+        'feeding_form': feeding_form
     })
 
 def add_photo(request, bird_id):
